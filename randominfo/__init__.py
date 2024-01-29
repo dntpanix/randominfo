95c03e0f02dbc0dd5a019518b22740d60b9d0063
VikiLopatniuk <viki.lopatniuk@gmail.com>
2024-01-29 09:48:15 +0000

    fixedhomework16

--- randominfo/__init__.py
+++ randominfo/__init__.py
@@ -269,7 +269,8 @@ def get_address():
 					allAddrs.append(addr[i])
 			except:
 				pass
-		full_addr.append(choice(allAddrs))
+		if (len(allAddrs) > 0):
+			full_addr.append(choice(allAddrs))
 	full_addr = dict(zip(addrParam, full_addr))
 	return full_addr
 
